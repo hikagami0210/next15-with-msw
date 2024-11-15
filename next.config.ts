525a3a220878ca2854d74968dfb4e361e6eb57d4
hikagami0210 <fsban772@gmail.com>
2024-11-15 09:30:52 +0900

    MSW setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const nextConfig: NextConfig & {
+  experimental: {
+    instrumentationHook: boolean;
+  };
+} = {
   /* config options here */
+  experimental: {
+    instrumentationHook: true,
+  },
 };
 
 export default nextConfig;
